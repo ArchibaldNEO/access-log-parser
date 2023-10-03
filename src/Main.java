969b2ce3db61599470571702a6208c8006827a9c
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-03 22:22:19 +0300

    feat: Переписал класс Main
    Необходимо было добавить считывание двух чисел и вычислить их сумму, разность, произведение, частное.
    
    МЕТА ДАННЫЕ: Задание по теме ""Синтаксис языка" и "Базовые типы данных""

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Введите текст и нажмите <Enter>:");
-    String text = new Scanner(System.in).nextLine();
-    System.out.println("Длина текста: " + text.length());
+  public static void main(String[] args) throws IOException {
+    System.out.println("Введите первое число:");
+
+    BufferedReader bufferedReader1 = new BufferedReader(new InputStreamReader(System.in));
+
+    String s1 = bufferedReader1.readLine(); //читаем строку с клавиатуры
+    int x1 = Integer.parseInt(s1);//преобразовываем строку в число.
+
+    System.out.println("Введите второе число:");
+
+    BufferedReader bufferedReader2 = new BufferedReader(new InputStreamReader(System.in));
+    String s2 = bufferedReader2.readLine(); //читаем строку с клавиатуры
+
+    int x2 = Integer.parseInt(s2);//преобразовываем строку в число.
+
+
+    System.out.println("Сумма чисел = " + (x1 + x2));
+    System.out.println("Разность чисел = " + (x1 - x2));
+    System.out.println("Произведение чисел = " + (x1 * x2));
+    double i = (double) x1 /x2;
+    System.out.println("Частное чисел = " + i);
+
   }
 }
