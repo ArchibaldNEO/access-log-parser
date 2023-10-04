e89693cddb6e6e2af021c4f4b463fbf4bbe16f06
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-04 13:50:19 +0300

    feat: Добавлен класс Number5.java
    Необходимо реализовать метод таким образом, чтобы он принимал число x и возвращал true если оно двузначное.
    
    МЕТА ДАННЫЕ: Практика Java. Классы и методы №5

---
+++ src/Number5.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number5 {
+  public static void main(String[] args) throws IOException {
+    System.out.print("Введите число: ");
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    String s = bufferedReader.readLine();
+    int d = Integer.parseInt(s);
+
+    System.out.println("Результат: " + isPositive(d));
+  }
+
+  public static boolean isPositive(int n) {
+    return (n <= -10 && n > -100) || (n >= 10 && n < 100);
+  }
+}
