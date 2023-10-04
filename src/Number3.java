2ad9b7ae301f95833ee294c918d288cf3c27e336
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-04 13:17:45 +0300

    feat: Добавлен класс Number3.java
    Необходимо реализовать метод таким образом, чтобы он преобразовывал символ в соответствующее число.
    
    МЕТА ДАННЫЕ: Практика Java. Классы и методы №3

---
+++ src/Number3.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number3 {
+  public static void main(String[] args) throws IOException {
+    System.out.print("Введите символ: ");
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    String s = bufferedReader.readLine();
+
+    System.out.println("Код символа ASCII " + " = " + charToNum(s.toCharArray()[0]));
+  }
+
+  public static int charToNum(char x) {
+    return x;
+  }
+}
