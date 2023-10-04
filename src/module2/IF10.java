ec29e1138deb0de6f3dc4906023e329397f53b03
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-04 22:34:41 +0300

    feat: Добавлен класс IF10.java
    Необходимо реализовать метод таким образом, чтобы он возвращал строку, в которой сначала будет число х, а затем одно из слов:
    
    год
    года
    лет
    
    МЕТА ДАННЫЕ: Практика Java. Условия №10

---
+++ src/module2/IF10.java
@@ -0,0 +1,26 @@
+package module2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class IF10 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Введите число x: ");
+    String s1 = bufferedReader.readLine();
+    int a = Integer.parseInt(s1);
+
+    System.out.println("Результат: " + age(a));
+  }
+
+  public static String age(int x) {
+    if ((x % 10 == 1) && (x != 11))
+      return x + " год";
+    else if (((x % 10 == 2) || (x % 10 == 3) || (x % 10 == 4)) && (x != 12) && (x != 13) && (x != 14)) {
+      return x + " года";
+    } else
+      return x + " лет";
+  }
+}
