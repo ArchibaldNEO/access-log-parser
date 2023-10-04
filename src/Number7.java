afdbdebf93d0d57272fa095b9abf5da8a92c7d46
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-04 16:31:37 +0300

    feat: Добавлен класс Number7.java
    Метод принимает левую и правую границу (a и b) некоторого числового диапазона. Необходимо реализовать метод таким образом, чтобы он возвращал true, если num входит в указанный диапазон (включая границы). Обратите внимание, что отношение a и b заранее неизвестно (неясно кто из них больше, а кто меньше)
    
    МЕТА ДАННЫЕ: Практика Java. Классы и методы №7

---
+++ src/Number7.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number7 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Введите [a: ");
+    String s1 = bufferedReader.readLine();
+    int a = Integer.parseInt(s1);
+
+    System.out.print("Введите b]: ");
+    String s2 = bufferedReader.readLine();
+    int b = Integer.parseInt(s2);
+
+    System.out.print("Введите num: ");
+    String s3 = bufferedReader.readLine();
+    int num = Integer.parseInt(s3);
+
+
+    System.out.println("Результат: " + isInRange(a, b, num));
+  }
+
+  public static boolean isInRange(int a, int b, int num) {
+    return (a <= num && num <= b) || (b <= num && num <= a);
+  }
+}
