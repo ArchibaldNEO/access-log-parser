75ff2ebe88fbe1dfd17a43a0e7dd3af57badf787
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-04 21:34:39 +0300

    feat: Добавлен класс IF2.java
    Необходимо реализовать метод таким образом, чтобы он возвращал деление x на y, и при этом гарантировал, что не будет выкинута ошибка деления на 0. При делении на 0 следует вернуть из метода число 0. Подсказка: смотри ограничения на операции типов данных.
    
    МЕТА ДАННЫЕ: Практика Java. Условия №2

---
+++ src/module2/IF2.java
@@ -0,0 +1,28 @@
+package module2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class IF2 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Введите число x: ");
+    String s1 = bufferedReader.readLine();
+    int a = Integer.parseInt(s1);
+
+    System.out.print("Введите число y: ");
+    String s2 = bufferedReader.readLine();
+    int b = Integer.parseInt(s2);
+
+    System.out.println("Результат: " + safeDiv(a, b));
+  }
+
+  public static int safeDiv(int x, int y) {
+    if (y == 0)
+      return 0;
+    else
+      return x / y;
+  }
+}
