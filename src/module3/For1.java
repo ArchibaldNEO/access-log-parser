aabcadbc15dd12372249a777f238e98784af6ac3
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-05 16:40:47 +0300

    feat: Добавлен класс For1.java
    Необходимо реализовать метод таким образом, чтобы он возвращал строку, в которой будут записаны все числа от 0 до x (включительно).
    
    МЕТА ДАННЫЕ: Практика Java. Циклы №1

---
+++ src/module3/For1.java
@@ -0,0 +1,25 @@
+package module3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class For1 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("x: ");
+    String s = bufferedReader.readLine();
+    int x = Integer.parseInt(s);
+
+    System.out.println("Результат: " + listNums(x));
+  }
+
+  public static String listNums(int x) {
+    String s = "";
+    for (int i = 0; i <= x; i++) {
+      s = s + i + " ";
+    }
+    return s;
+  }
+}
