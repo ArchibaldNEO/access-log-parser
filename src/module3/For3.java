c4c7cb2fbc0d10ce8aee15e125760c19a0417293
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-05 20:19:58 +0300

    feat: Добавлен класс For3.java
    Необходимо реализовать метод таким образом, чтобы он возвращал строку, в которой будут записаны все четные числа от 0 до x (включительно). Подсказа для обеспечения качества кода: инструкцию if использовать не следует.
    
    МЕТА ДАННЫЕ: Практика Java. Циклы №3

---
+++ src/module3/For3.java
@@ -0,0 +1,26 @@
+package module3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class For3 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("x: ");
+    String s = bufferedReader.readLine();
+    int x = Integer.parseInt(s);
+
+    System.out.println("Результат: " + chet(x));
+  }
+
+  public static String chet(int x) {
+    String s = "";
+    for (int i = 0; i <= x; i = i + 2) {
+      s = s + i + " ";
+
+    }
+    return s;
+  }
+}
