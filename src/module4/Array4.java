8648bdd64d5a2dd69999b0b6001ac55d57aa0ca0
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-09 09:52:48 +0300

    feat: Добавлен класс Array4.java
    Необходимо реализовать метод таким образом, чтобы он возвращал количество положительных элементов массива arr.
    
    МЕТА ДАННЫЕ: Практика Java.Массивы 4

---
+++ src/module4/Array4.java
@@ -0,0 +1,35 @@
+package module4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Array4 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Введите длину массива: ");
+    String s1 = bufferedReader.readLine();
+    int n = Integer.parseInt(s1);
+    int[] array = new int[n];
+    System.out.println("Введите элементы массива:");
+    for (int i = 0; i < n; i++) {
+      String s2 = bufferedReader.readLine();
+      array[i] = Integer.parseInt(s2);
+    }
+    System.out.println(Arrays.toString(array));
+
+    System.out.println("Результат: " + maxAbs(array));
+  }
+
+  public static int maxAbs(int[] arr) {
+    int positive = 0;
+    for (int j : arr) {
+      if (j > 0)
+        positive++;
+    }
+    return positive;
+  }
+
+}
