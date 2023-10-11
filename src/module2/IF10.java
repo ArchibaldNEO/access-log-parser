076c0e18a5f729cf2e6199ff1677b0412a5f929b
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-11 13:49:08 +0300

    fix: Исправил задание 10 по условиям

--- src/module2/IF10.java
+++ src/module2/IF10.java
@@ -16,9 +16,9 @@ public class IF10 {
   }
 
   public static String age(int x) {
-    if ((x % 10 == 1) && (x != 11))
+    if ((x % 10 == 1) && (x / 11 == 0))
       return x + " год";
-    else if (((x % 10 == 2) || (x % 10 == 3) || (x % 10 == 4)) && (x != 12) && (x != 13) && (x != 14)) {
+    else if (((x % 10 == 2) || (x % 10 == 3) || (x % 10 == 4)) && (x / 12 == 0) && (x / 13 == 0) && (x / 14 == 0)) {
       return x + " года";
     } else
       return x + " лет";
