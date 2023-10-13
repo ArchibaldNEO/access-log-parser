cf2c35245a515d97325a596094ed15903f69d7ce
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-13 11:37:46 +0300

    fix: Объекты и классы №1

--- src/module5/ex1/Point.java
+++ src/module5/ex1/Point.java
@@ -1,18 +1,11 @@
 package module5.ex1;
 
-import java.util.Objects;
-
 public class Point {
   int X, Y;
 
   public Point(int x, int y) {
     this.X = x;
     this.Y = y;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(X, Y);
   }
 
   @Override
