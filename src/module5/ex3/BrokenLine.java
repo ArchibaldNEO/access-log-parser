5d9f6246dbedc72b0ae4043c7c18354930e6d65b
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-13 23:46:20 +0300

    feat: Объекты и классы №3

---
+++ src/module5/ex3/BrokenLine.java
@@ -0,0 +1,37 @@
+package module5.ex3;
+
+import module5.ex1.Point;
+import module5.ex2.Line;
+
+import java.util.Arrays;
+
+public class BrokenLine {
+  Point[] array;
+
+  public BrokenLine(Point[] array) {
+    this.array = array;
+  }
+
+  public double lenght() {
+    double lenght = 0;
+    for (int i = 0; i < array.length - 1; i++) {
+      Line line = new Line(array[i], array[i + 1]);
+      lenght = lenght + line.length();
+    }
+    return lenght;
+  }
+
+  public Line[] arrayLine() {
+    Line[] lines = new Line[array.length - 1];
+    for (int i = 0; i < array.length - 1; i++) {
+      lines[i] = new Line(array[i], array[i + 1]);
+
+    }
+    return lines;
+  }
+
+  @Override
+  public String toString() {
+    return "Линия " + Arrays.toString(array);
+  }
+}
