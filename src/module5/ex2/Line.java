ead4c57bc457f3e12741b60455e7e6a2c2c607f1
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-30 11:51:22 +0300

    fix: Line.java

--- src/module5/ex2/Line.java
+++ src/module5/ex2/Line.java
@@ -11,6 +11,11 @@ public class Line {
     this.end = end;
   }
 
+  public Line(int x1, int y1, int x2, int y2) {
+    this.begin = new Point(x1, y1);
+    this.end = new Point(x2, y2);
+  }
+
   @Override
   public String toString() {
     return "Линия от " + begin + " до " + end;
