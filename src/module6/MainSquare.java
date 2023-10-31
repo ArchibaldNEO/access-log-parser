74c00eecbb7ee997f566beb760d843ae2c3b1fe3
ArchibaldNEO <skvortcov1996@gmail.com>
2023-10-31 13:51:34 +0300

    feat: Инкапсуляция №2. Квадрат

---
+++ src/module6/MainSquare.java
@@ -0,0 +1,16 @@
+package module6;
+
+import java.io.IOException;
+
+public class MainSquare {
+  public static void main(String[] args) throws IOException {
+    Square square = new Square(1, 5, 10);
+    System.out.println(square);
+
+    square.setX(1);
+    square.setY(1);
+    square.setLen(1);
+    System.out.println(square);
+
+  }
+}
