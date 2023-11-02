8c88421128f2c14cf46bd64289a1bcc600969ec9
ArchibaldNEO <skvortcov1996@gmail.com>
2023-11-02 15:09:03 +0300

    fix: Полиморфизм №2. Замкнутая ломаная

--- src/module7/MainTest.java
+++ src/module7/MainTest.java
@@ -10,12 +10,7 @@ public class MainTest {
     //Polymorphism1 polymorphism1 = new Polymorphism1(1, 24, 5);
     //System.out.println(polymorphism1);
 
-    Polymorphism2 polymorphism2 = new Polymorphism2(new Point[]{new Point(0, 0), new Point(0, 10),
-            new Point(5, 10), new Point(5, 0), new Point(0, 0)});
 
-    System.out.println(polymorphism2);
-
-    System.out.println(polymorphism2.lenght());
 
 
   }
