04c8d2d76f67e35341a21805bd409cdc50e8b2e5
ArchibaldNEO <skvortcov1996@gmail.com>
2023-11-07 10:19:51 +0300

    feat: Object #1. Сравнение дробей

--- src/ru/courses/oop/polymorphisms/Fraction.java
+++ src/ru/courses/oop/polymorphisms/Fraction.java
@@ -1,11 +1,38 @@
 package ru.courses.oop.polymorphisms;
 
-public class Fraction extends Number{
+import java.util.Objects;
+
+public class Fraction extends Number implements Cloneable {
   int num, denuml;
 
   public Fraction(int num, int denuml) {
     this.num = num;
     this.denuml = denuml;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    Fraction fraction = (Fraction) o;
+    return num == fraction.num && denuml == fraction.denuml;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(num, denuml);
+  }
+
+  @Override
+  public Fraction clone() {
+    try {
+      // TODO: copy mutable state here, so the clone can't change the internals of the original
+      return (Fraction) super.clone();
+    } catch (CloneNotSupportedException e) {
+      throw new AssertionError();
+    }
   }
 
   @Override
@@ -15,22 +42,23 @@ public class Fraction extends Number{
 
   @Override
   public int intValue() {
-    return num/denuml;
+    return num / denuml;
   }
 
   @Override
   public long longValue() {
-    return num/denuml;
+    return num / denuml;
   }
 
   @Override
   public float floatValue() {
-    return (float) num /denuml;
+    return (float) num / denuml;
   }
 
   @Override
   public double doubleValue() {
-    return (double) num /denuml;
+    return (double) num / denuml;
   }
+
 }
 
--- src/ru/courses/oop/polymorphisms/MainTest.java
+++ src/ru/courses/oop/polymorphisms/MainTest.java
@@ -1,7 +1,5 @@
 package ru.courses.oop.polymorphisms;
 
-import java.io.IOException;
-
 public class MainTest {
   public static void main(String[] args) throws Exception {
     /*Polymorphism1 polymorphism1 = new Polymorphism1(1, 24, 5);
@@ -20,7 +18,7 @@ public class MainTest {
     System.out.println(fraction.doubleValue());*/
 
 
-    Point point1 = new Point(3, 4);
+   /* Point point1 = new Point(3, 4);
     Point point2 = new Point(3, 4);
     Point point3 = point2.clone();
 
@@ -28,8 +26,21 @@ public class MainTest {
     System.out.println("point1.equals(point2) - " + point1.equals(point2));
     System.out.println("----------");
     System.out.println("point2 == point3 - " + (point2 == point3));
-    System.out.println("point2.equals(point3) - " + point2.equals(point3));
+    System.out.println("point2.equals(point3) - " + point2.equals(point3));*/
 
+    Fraction fraction1 = new Fraction(3, 4);
+    Fraction fraction2 = new Fraction(3, 4);
+    Fraction fraction3 = fraction2.clone();
+
+
+    System.out.println("== : " + (fraction1 == fraction2));
+    System.out.println("equals : " + (fraction1.equals(fraction2)));
+    System.out.println(fraction3);
+
+    fraction1.num = 4;
+    fraction2.num = 4;
+    System.out.println("equals : " + (fraction1.equals(fraction2)));
+    System.out.println(fraction3);
 
   }
 }
