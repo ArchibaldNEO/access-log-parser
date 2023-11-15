a73447d9d88d8d7bd6d81d7762b57457166d4e39
ArchibaldNEO <skvortcov1996@gmail.com>
2023-11-15 13:50:06 +0300

    feat: Исключения #1. NumberFormatException

---
+++ src/ru/courses/oop/exceptions/Sum.java
@@ -0,0 +1,17 @@
+public class Sum {
+  public static void main(String[] args) {
+    double sum = 0;
+    double d = 0;
+    for (int i = 0; i < args.length; i++) {
+      try {
+        d = Double.parseDouble(args[i]);
+      } catch (NumberFormatException e) {
+        System.out.println(e);
+        d = 0;
+      }
+      sum += d;
+    }
+    System.out.println(sum);
+  }
+}
+
