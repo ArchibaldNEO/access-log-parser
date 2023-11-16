24792b1c23def72299e1cca6208e083ddd17ebff
ArchibaldNEO <skvortcov1996@gmail.com>
2023-11-16 09:58:11 +0300

    fix: Исключения #2.

--- src/ru/courses/oop/exceptions/Sum.java
+++ src/ru/courses/oop/exceptions/Sum.java
@@ -1,9 +1,16 @@
-public class Sum {
-
-  public static void main(String[] args) throws NumberFormatException {
+public class Sum
+{
+  public static void main(String[] args) {
     double sum = 0;
+    double d = 0;
     for (int i = 0; i < args.length; i++) {
-      sum += Double.parseDouble(args[i]);
+      try {
+        d = Double.parseDouble(args[i]);
+      } catch (NumberFormatException e) {
+        System.out.println(e);
+        d = 0;
+      }
+      sum += d;
     }
     System.out.println(sum);
   }
