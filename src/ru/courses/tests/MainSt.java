434cdc13095ae3b4baa4d654cb0611218fa66768
ArchibaldNEO <skvortcov1996@gmail.com>
2023-11-24 12:47:57 +0300

    feat: Тестирование студента 1) Исправить инкапсуляцию для списка оценок

---
+++ src/ru/courses/tests/MainSt.java
@@ -0,0 +1,25 @@
+package ru.courses.tests;
+
+import java.io.IOException;
+
+public class MainSt {
+  public static void main(String[] args) throws IOException {
+    Student student = new Student("Arte");
+    student.setName("Artem");
+    System.out.println(student.getName());
+
+    student.addGrade(2);
+    student.addGrade(3);
+    student.addGrade(4);
+    student.addGrade(5);
+
+   System.out.println(student.getGrades());
+    //student.addGrade(1);
+    //student.addGrade(6);
+    System.out.println(student.getGrades().set(0,1));
+    System.out.println(student.getGrades().set(2,9));
+    student.addGrade(7);
+
+    System.out.println(student);
+  }
+}
