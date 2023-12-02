c0098aed94625a814025ba7bf596de3f9facb524
ArchibaldNEO <skvortcov1996@gmail.com>
2023-12-03 00:11:16 +0300

    fix: Пакеты 3

--- src/ru/courses/oop/packages/MainPow.java
+++ src/ru/courses/oop/packages/MainPow.java
@@ -2,13 +2,8 @@ import java.io.IOException;
 
 public class MainPow {
   public static void main(String[] args) throws IOException {
-    for (int i = 0; i < 1; i++) {
-      System.out.println("Результат возведения X в степень Y: " + pow(args[0], args[1]));
-    }
-  }
-
-  public static int pow(String x, String y) {
-    return (int) Math.pow(Integer.parseInt(x), Integer.parseInt(y));
+    System.out.println("Результат возведения X в степень Y: " +
+            Math.pow(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
   }
 }
 
