c3913c4764a94a917a5b88116b8543eb9e73304e
ArchibaldNEO <skvortcov1996@gmail.com>
2023-12-25 14:49:49 +0300

    feat: JDBC

---
+++ src/main/java/edu/innotech/jdbc/Service.java
@@ -0,0 +1,34 @@
+package edu.innotech.jdbc;
+
+
+import java.sql.*;
+
+public class Service {
+  public static void main(String[] args) {
+    createDB();
+  }
+
+  public static void createDB() {
+    String url = "jdbc:h2:.\\Office";
+    String url1 = "jdbc:h2://localhost:5432//Office";
+    try (Connection connection = DriverManager.getConnection(url)) {
+      if (connection != null) {
+        System.out.println("Good");
+      } else {
+        System.out.println("Fail");
+      }
+
+      Statement statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery("select* from Department");
+      //System.out.println(resultSet);
+      while (resultSet.next()) {
+        System.out.println(resultSet.getInt("DepartmentID"));
+      }
+    } catch (SQLException ex) {
+      System.out.println(ex);
+    }
+  }
+
+}
+
+
